6722ec4e27bd66449f1def0fdd86e41edf3e8eb4
munhofU <munhofdev@gmail.com>
2023-10-09 08:44:15 -0300

    algo

--- clase 14/clase142.py
+++ clase 14/clase142.py
@@ -7,6 +7,7 @@ Created on Mon Oct  2 11:24:08 2023
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 pinguinos = pd.read_csv("penguins_size.csv")
 
@@ -14,9 +15,11 @@ pinguinos.describe()
 
 fig, axs = plt.subplots(2, 2)
 axs[0,0].hist(pinguinos["body_mass_g"],bins="sturges")
+axs[0,0].kde(pinguinos["body_mass_g"])
 axs[0,1].hist(pinguinos["culmen_length_mm"],bins="sturges")
 axs[1,0].hist(pinguinos["culmen_depth_mm"],bins="sturges")
 axs[1,1].hist(pinguinos["flipper_length_mm"],bins="sturges")
+plt.legend()
 plt.show()
 plt.close()
 
@@ -32,3 +35,23 @@ pringuinos_male_adelie = pinguinos_male[filtro_especie]
 pringuinos_male_adelie.describe()
 
 
+islas = pd.unique(pinguinos.island)
+
+cantidad_de_pinguinos = pinguinos["island"].value_counts()
+
+
+sns.lmplot(data=pinguinos,x="culmen_length_mm", y="culmen_depth_mm", hue="sex")
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
